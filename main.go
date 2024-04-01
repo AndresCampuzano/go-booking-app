92f0464c234165347f0eaaf019cc476ffc1f7456
Andres Campuzano Garzon <campuxano88@gmail.com>
2024-03-31 19:10:13 -0500

    go practice

--- main.go
+++ main.go
@@ -3,5 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Initial commit")
+	conferenceName := "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickers uint = 50
+	bookings := []string{}
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickers)
+	fmt.Println("Get your tickets here to attend.")
+
+	fmt.Println()
+
+	for {
+		var userName string
+		var userTickets uint
+
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&userName)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		if userTickets > remainingTickers {
+			fmt.Printf("There are only %v tickets available, so you can't book %v tickets.\n", remainingTickers, userTickets)
+			continue
+		}
+
+		remainingTickers = remainingTickers - userTickets
+		bookings = append(bookings, userName)
+
+		fmt.Printf("Thank you %v for booking %v tickets.\n", userName, userTickets)
+		fmt.Printf("There are %v tickets remaining.\n", remainingTickers)
+
+		fmt.Printf("All bookings: %v\n", bookings)
+
+		if remainingTickers == 0 {
+			fmt.Println("All tickets have been booked.")
+			break
+		}
+	}
 }
